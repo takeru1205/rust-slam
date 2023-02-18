99fb6be8f968aaed6abdbc4ddca9170fe89f2ac8
takeru1205 <takeru0639418@gmail.com>
2023-02-18 17:43:02 +0900

    [update] play video

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use opencv::{core, highgui, prelude::*, videoio};
+
+fn run() -> opencv::Result<()> {
+    let window = "video capture";
+    highgui::named_window(window, 1)?;
+
+    let file_name = "test.mp4";
+    let mut cam = videoio::VideoCapture::from_file(&file_name, videoio::CAP_ANY)?;
+    let opened_file = videoio::VideoCapture::open_file(&mut cam, &file_name, videoio::CAP_ANY)?;
+    if !opened_file {
+        panic!("Unable to open video file!");
+    };
+    let mut frame = core::Mat::default();
+    let frame_read = videoio::VideoCapture::read(&mut cam, &mut frame)?;
+    if !frame_read {
+        panic!("Unable to read from video file!");
+    };
+    let opened = videoio::VideoCapture::is_opened(&mut cam)?;
+    println!("Opened? {}", opened);
+    if !opened {
+        panic!("Unable to open video file!");
+    };
+
+    loop {
+        videoio::VideoCapture::read(&mut cam, &mut frame)?;
+        if frame.size()?.width > 0 {
+            highgui::imshow(window, &frame)?;
+            let key = highgui::wait_key(10)?;
+            if key > 0 && key != 255 {
+                videoio::VideoCapture::release(&mut cam)?;
+                break;
+            }
+        } else {
+            println!("No more frames!");
+            videoio::VideoCapture::release(&mut cam)?;
+            break ();
+        }
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    run().unwrap()
 }
